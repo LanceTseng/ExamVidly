eade7ea114227d97d8db9bc6822e87ca1ac3bb2c
YuFen <lance_tseng@bizlinktech.com>
2021-10-14 15:04:13 +0800

    override convention 1. modify model 2. add some attr

--- ExamVidly/Models/Customer.cs
+++ ExamVidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Web;
@@ -9,6 +10,8 @@ namespace ExamVidly.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
