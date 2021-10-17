179e358469c3e176df0e54f6737786684c380cb2
YuFen <lance_tseng@bizlinktech.com>
2021-10-17 10:16:35 +0800

    1. add new model Genre 2. modify model Movie

--- ExamVidly/Models/Movie.cs
+++ ExamVidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,15 @@ namespace ExamVidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         public string  Name { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime ReleasedDate { get; set; }
+
+        public int NumberInStock { get; set; }
     }
 }
