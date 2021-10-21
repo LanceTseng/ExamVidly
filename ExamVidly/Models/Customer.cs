d9ae5149c8465b801dddf3f4c93d0f9d848bef7f
YuFen <lance_tseng@bizlinktech.com>
2021-10-21 23:13:24 +0800

    change display name and format

--- ExamVidly/Models/Customer.cs
+++ ExamVidly/Models/Customer.cs
@@ -5,17 +5,23 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.ConstrainedExecution;
 using System.Web;
+using Microsoft.Ajax.Utilities;
 
 namespace ExamVidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
