1e6ef15d6f06ba300c2415fbdc48cdb02f3fd6c4
YuFen <lance_tseng@bizlinktech.com>
2021-10-24 17:55:21 +0800

    anti-forgery tokens 1. add Html.AntiForgeryToken to View 2. add Attr ValidateAntiForgeryToken to Controller

--- ExamVidly/Controllers/CustomersController.cs
+++ ExamVidly/Controllers/CustomersController.cs
@@ -40,6 +40,7 @@ namespace ExamVidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)
