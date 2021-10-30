09a92db13bf5e06236beb21e1faa44c8f6cbe5d8
YuFen <tsengyf.0914@g.ncu.edu.tw>
2021-10-30 23:27:24 +0800

    working with roles 1. use User.IsInrole to controller 2. coby Index and rename to List (List / ReadOnlyList) 3. add class RoleName to replace magic code

---
+++ ExamVidly/Models/RoleName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamVidly.Models
+{
+    public class RoleName
+    {
+        public const string CanManageMovies = "CanManageMovies";
+    }
+}
