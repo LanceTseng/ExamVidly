29e2e3b54a30ba9032b42688506ef3dca6eed7af
YuFen <tsengyf.0914@g.ncu.edu.tw>
2021-11-01 13:48:18 +0800

    Social Login FB 1. press F4 to enable SSL to get the port 2. open properties to Tab Web past replace the older 3. go FB developer to create APP to get the id and app key 4. use UseFacebookAuthentication to paste the info

--- ExamVidly/App_Start/FilterConfig.cs
+++ ExamVidly/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace ExamVidly
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
