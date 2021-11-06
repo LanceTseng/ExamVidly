d11522f40196a6edeca4201af4fbdb4311be2419
YuFen <tsengyf.0914@g.ncu.edu.tw>
2021-11-06 10:24:40 +0800

    add NewRental api controller and RentalDto

---
+++ ExamVidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ExamVidly.Dtos;
+
+namespace ExamVidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateRentals(NewRentalDto rentalDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
